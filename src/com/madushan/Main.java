af9f53f059bb06dd6ce796ba5e0c23ab717c6a1c
Madushan Nipuna Shantharathna <66712958+Madushan97@users.noreply.github.com>
2022-12-16 22:18:10 +0530

    added conditions

--- src/com/madushan/Main.java
+++ src/com/madushan/Main.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 public class Main {
 
-
     public static void main(String[] array) {
 
         int top = 0;
@@ -18,9 +17,9 @@ public class Main {
         String[] userSplitArray = userInput.split("", userInput.length());
 
 //        print the split array vertically
-        for (String n : userSplitArray) {
-            System.out.println(n);
-        }
+//        for (String n : userSplitArray) {
+//            System.out.println(n);
+//        }
 
 //        creat a string stack
         String[] stack = new String[userSplitArray.length];
@@ -30,12 +29,46 @@ public class Main {
 //        userSplitArray elements into the stack one by one
         for(int i=0; i < userSplitArray.length; i++) {
              stack[top] = userSplitArray[i];
+             top++;
         }
 
+//        print the stack
+//        for(String n : stack) {
+//            System.out.println(n);
+//        }
 
-
-
+//        conditions checking
+        for (int j=0; j < stack.length; j++) {
+            switch (stack[j]) {
+//                case (
+                case "(" :
+//                    if 0 index has (
+                    if (stack[j] == "(") {
+//                        checking  whether ) is there or not
+                        for (int k=0; k < stack.length; k++) {
+                            if(stack[k+1] == ")"){
+                                System.out.println(") found");
+                            }
+                        }
+                    }
+                case "[" :
+                    if (stack[j] == "[") {
+                        for (int k=0; k < stack.length; k++) {
+                            if(stack[k+1] == "]"){
+                                System.out.println("] found");
+                            }
+                        }
+                    }
+                case "{" :
+                    if (stack[j] == "{") {
+                        for (int k=0; k < stack.length; k++) {
+                            if(stack[k+1] == "}"){
+                                System.out.println("} found");
+                            }
+                        }
+                    }
+            }
+        }
     }
-
 
 }
