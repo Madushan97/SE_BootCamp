c4e81c3f6ef32f13380c0b56cfdab8ee97bd2e19
Madushan Nipuna Shantharathna <66712958+Madushan97@users.noreply.github.com>
2022-12-20 12:54:35 +0530

    bubble sort algorith

--- src/com/madushan/Main.java
+++ src/com/madushan/Main.java
@@ -3,6 +3,18 @@ package com.madushan;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        int numberArray[] = {47, 84, 75, 21, 14, 14, 79};
+
+//        create an instance of SortAndMath class
+        var sort = new SortAndMath();
+        sort.bubbleSort(numberArray);
+
+//        print the sorted array
+        for(int i: numberArray) {
+            System.out.print(i + ", ");
+        }
     }
+
+
 }
