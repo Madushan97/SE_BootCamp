3333c07f99d5b00ff2099a1f190b2458b3064f1e
Madushan Nipuna Shantharathna <66712958+Madushan97@users.noreply.github.com>
2022-12-23 07:56:33 +0530

    initial commit

--- src/com/madushan/Main.java
+++ src/com/madushan/Main.java
@@ -3,44 +3,6 @@ package com.madushan;
 public class Main {
 
     public static void main(String[] args) {
-
-        int numberArray[] = {47, 84, 75, 21, 14, 14, 79};
-
-//  create an instance of SortAndMath class and Math
-        var sort = new SortAndMath();
-        Math highestVal = new Math();
-        Math primeNumber = new Math();
-
-//  sort the array
-        sort.bubbleSort(numberArray);
-
-//  print the sorted array
-        for(int i: numberArray) {
-            System.out.print(i + ", ");
-        }
-        System.out.println();
-
-//  print the median
-        Median(numberArray);
-
-//  Highest Values
-        int high = highestVal.Highest_Value(numberArray);
-        System.out.println("Highest Value is : " + high);
-
-//  Mode
-        highestVal.Mode(numberArray);
-
-//    prime numbers
-        primeNumber.PrimeNumber(numberArray);
+        System.out.println("Assignment 4");
     }
-
-//  for median
-    public static void Median(int array[]) {
-        int arraySize = array.length;
-        int a[] = array;
-        float median = a[((arraySize+1)/2)-1];
-        System.out.println("Median is : " + median);
-    }
-
-
 }
