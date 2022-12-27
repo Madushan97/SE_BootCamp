74f4633e11b010e1383caebe87996880afe717bd
Madushan Nipuna Shantharathna <66712958+Madushan97@users.noreply.github.com>
2022-12-27 09:05:45 +0530

    BufferedReader BufferedWriter

--- src/com/madushan/Main.java
+++ src/com/madushan/Main.java
@@ -1,8 +1,31 @@
 package com.madushan;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Assignment 4");
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
+            writer.write("Text writing - Test 1");
+            writer.write("\nHello World- Test 1");
+            writer.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("output.txt"));
+            String line;
+            while((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
