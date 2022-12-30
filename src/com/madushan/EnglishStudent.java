130afe6f70df87e1aea38b26c94d72fdc2f7c5e8
Madushan Nipuna Shantharathna <66712958+Madushan97@users.noreply.github.com>
2022-12-30 22:08:08 +0530

    Grading system done for English

--- src/com/madushan/EnglishStudent.java
+++ src/com/madushan/EnglishStudent.java
@@ -18,7 +18,26 @@ public class EnglishStudent extends Student {
                 String[] English = new String[30];
                 English = line.split(" ", 30);
                 if(English[0].equals("English")) {
-                    System.out.println(line);
+//                    counting the Average marks
+                    int termPaper = ((Integer.parseInt(English[1])*30)/100);
+                    int midTerm = ((Integer.parseInt(English[2])*30)/100);
+                    int finalExam = ((Integer.parseInt(English[3])*40)/100);
+                    int avgEnglish = (termPaper + midTerm + finalExam);
+
+//                    Grade
+                    if(avgEnglish > 90) {
+                        System.out.println("Grade A for English and you have got " + avgEnglish);
+                    } else if (avgEnglish > 80 && avgEnglish <=90 ) {
+                        System.out.println("Grade B for English and you have got " + avgEnglish);
+                    } else if (avgEnglish >70 && avgEnglish <=80 ) {
+                        System.out.println("Grade C for English and you have got " + avgEnglish);
+                    } else if (avgEnglish > 60 && avgEnglish <= 70 ) {
+                        System.out.println("Grade S for English and you have got " + avgEnglish);
+                    } else if (avgEnglish > 50 && avgEnglish <= 60 ) {
+                        System.out.println("Grade D for English and you have got " + avgEnglish);
+                    } else if (avgEnglish > 40 && avgEnglish <= 50 ) {
+                        System.out.println("Grade E for English and you have got " + avgEnglish);
+                    }
                 }
             }
             reader.close();
